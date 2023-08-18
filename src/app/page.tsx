950e166333de37d38b0e19d3998d31274766e8f4
AnCoNy2001 <buihoangan0501@gmail.com>
2023-08-18 08:52:55 +0700

    Set up prettierrc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Home: React.FC = () => {
-  return (
-    <div>Hello home page</div>
-  );
-};
+  return <div>Hello home page</div>
+}
 
-export default Home;
+export default Home
