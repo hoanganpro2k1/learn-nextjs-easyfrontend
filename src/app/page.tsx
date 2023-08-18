d4766dd6e31231f0cfca76e2f4fea385abe07206
AnCoNy2001 <buihoangan0501@gmail.com>
2023-08-18 09:05:40 +0700

    Update SEO title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
 const Home: React.FC = () => {
-  return <div>Hello home page</div>
+  return (
+    <div>
+      <a href="/">Learn next js an bui</a>
+    </div>
+  )
 }
 
 export default Home
